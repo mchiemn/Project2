972d78afdaa76372c02cb7eb7eda3d2f28010e13
Ero V <erovdr@gmail.com>
2021-11-18 03:30:19 -0800

    Completed wiki crawler + data
    Wiki crawler complete with raw and filtered data in the data folder. pagerank.py includes a function to read a .csv file and build an appropriate dictionary

--- crawler.py
+++ crawler.py
@@ -3,20 +3,21 @@ from bs4 import SoupStrainer
 import requests
 import random
 import time
-import re   
+import re
 import cchardet
 
-
+# Page objects
+# Uses dictionary for O(1) lookup when calculating pagerank
 class page:
     def __init__(self, outlinks):
         self.outlinks = outlinks
-        self.outlinksList = []
+        self.outlinksDict = {}
 
 def scrapeCPP():
     content = requests.get('https://www.cpp.edu/').content
     # Minimum number of links to crawl
     crawled = 0
-    target = 2
+    target = 5
 
     # Set of links we could crawl
     set_of_outlinks = set(())
@@ -30,6 +31,8 @@ def scrapeCPP():
         # Create new page object for the page we are crawling
         newPage = page(0)
 
+        # Counter variable that only serves to create the page object's dictionary
+        counter = 0
         # Crawl through the page and only get links that are 'cpp.edu'
         for link in BeautifulSoup(content, parse_only = SoupStrainer('a', href=re.compile('cpp.edu/')), features='lxml'):
             if hasattr(link, 'href') and link['href'].startswith('https'):
@@ -41,15 +44,14 @@ def scrapeCPP():
                 # If the link we find is not already crawled, add it to the set of possible links we can crawl
                 if(link['href'] not in linksCrawled):
                     set_of_outlinks.add(link['href'])
-                
-                # Change this to a dictionary for fast lookup instead of list
-                # key is link, value is just index (value doesnt matter)
-                newPage.outlinksList.append(link['href'])
+
+                # Add the link to the page object's dictionary
+                newPage.outlinksDict.update({link['href'] : counter})
 
         # If the set of links we can crawl is empty, then stop crawling
         if len(set_of_outlinks) == 0:
             break
-        
+
         while True:
             try:
                 # Get new link from set of outlinks and set content for next loop
@@ -73,8 +75,23 @@ def scrapeCPP():
         time.sleep(1.5)
     end = time.time()
 
+
+    # For loop to remove any links that are not a part of the links that are crawled, keep lists within crawled links
+    # For each page object that we have crawled
+    for value in linksCrawled.values():
+        currentDict = value.outlinksDict
+        # For each key in the page's outlink dictionary
+        for key in list(currentDict.keys()):
+            # If that key is a URL we have not crawled, remove it
+            if key not in linksCrawled:
+                del value.outlinksDict[key]
+        # Update number of outlinks for this page object
+        value.outlinks = len(value.outlinksDict)
+
+    # Prints the link, the number of outlinks it has, and what those outlinks are
+    # Comment out for large crawls please
     for key, value in linksCrawled.items():
-        print(key, value.outlinks)
+        print(key, value.outlinks, value.outlinksDict)
     print(end - start)
 
 
--- wikicrawler.py
+++ wikicrawler.py
@@ -3,95 +3,143 @@ import random
 import time
 
 
-class Page:
+KEY_TERM = 'Filipino actress'
+TARGET = 700
+FILE_OUT = 'data/wiki-repo.csv'
+FILE_FILTER = 'data/wiki-repo-filtered.csv'
+
+
+class PageOutlinks:
     def __init__(self, outlinks):
+        self.outlinks_li = outlinks
         self.outlinks = len(outlinks)
-        self.outlinks_dict = {}
-        for i in outlinks:
-            self.outlinks_dict[i] = 0
+        # removes commas for .CSV consistency
+        for i in range(0, len(outlinks)):
+            self.outlinks_li[i] = self.outlinks_li[i].replace(',', ' ')
+
+
+# Converts list of outlinks into a CSV string, then writes data to file
+def write_to_file(filename, links_dict):
+    dict_keys = list(links_dict.keys())
+    # creates list of dictionary keys
+
+    # creates first line - overwrites existing data
+    outlinks_str = ''
+    if links_dict[dict_keys[0]].outlinks != 0:
+        for i in links_dict[dict_keys[0]].outlinks_li:
+            outlinks_str += i + ', '
+            outlinks_str = outlinks_str[:-1]
+            outlinks_str = outlinks_str[:-1]
+    with open(filename, 'w') as f:
+        f.write('%s,%s,%s\n' % (dict_keys[0], links_dict[dict_keys[0]].outlinks, outlinks_str))
+
+    # appends remaining lines
+    for i in range(1, len(links_dict)):
+        outlinks_str = ''
+        if links_dict[dict_keys[i]].outlinks != 0:
+            for j in links_dict[dict_keys[i]].outlinks_li:
+                outlinks_str += j + ', '
+                outlinks_str = outlinks_str[:-1]
+                outlinks_str = outlinks_str[:-1]
+        with open(filename, 'a') as f:
+            f.write('%s,%s,%s\n' % (dict_keys[i], links_dict[dict_keys[i]].outlinks, outlinks_str))
+
+
+# removes outlinks that do not point to any indexes
+def strip_dict(outlinks_dict):
+    dict_keys = list(outlinks_dict.keys())
+    new_dict = {}
+
+    for i in range(0, len(outlinks_dict)):
+        active_links =[]
+        for j in outlinks_dict[dict_keys[i]].outlinks_li:
+            if j in dict_keys:
+                active_links.append(j)
+        new_page = PageOutlinks(active_links)
+        new_dict[dict_keys[i]] = new_page
+    return new_dict
 
 
 def scrape_wiki():
     # Minimum number of links to crawl
     crawled = 0
-    target = 10
+    target = TARGET
 
     # Dictionary stores all the links that we have crawled
-    # Key = URL, Value = Page object
+    # Key = URL, Value = WikiOutlinks object
     links_crawled = {}
 
     # Set of links we could crawl
     set_of_outlinks = set(())
 
     # Initial key term
-    key_term = 'Tropical fish'
-    search_term = key_term
-
-    # Begin crawl
-    while crawled < target:
+    search_term = KEY_TERM
+    try:
+        wiki = wikipedia.page(KEY_TERM)
+    except wikipedia.exceptions.PageError as e:
         li = wikipedia.search(search_term, 2)
-        # returns a list of relevant terms in the Wiki database
         try:
             wiki = wikipedia.page(li[0])
             # try to parse data in first search result
         except wikipedia.exceptions.DisambiguationError as e:
             wiki = wikipedia.page(li[1])
-            # if the first search result is too vague, the parser will return an error
-            # so, the second result is selected (see Wikipedia API documentation)
-        except wikipedia.exceptions.PageError as e:
-            li = wikipedia.search(key_term, 2)
-            try:
-                wiki = wikipedia.page(li[0])
-            except wikipedia.exceptions.DisambiguationError as e:
-                wiki = wikipedia.page(li[1])
-            # restarts search if null search i.e. a Wikipedia page on
-            # search term does not yet exist (rare)
+        # if the first search result is too vague, the parser will return an error
+        # so, the second result is selected (see Wikipedia API documentation)
+    print("Search: " + wiki.title)
 
-        print('Page: ' + wiki.title)
-        #print('Outlinks: ' + str(len(wiki.links)))
-        print('Crawled: ' + str(len(links_crawled)))
-        new_page = Page(wiki.links)
-        # 'links' function a list of internal links parsed from the page
+    # Build set - Assumes initial search contains a large amount of outlinks
+    # Here, we begin with a Wiki category
+    set_of_outlinks.update(wiki.links)
+    print("Set size: " + str(len(set_of_outlinks)))
 
-        # Add to dictionary
-        if wiki.title not in links_crawled:
-            for a in wiki.links:
-                if a not in set_of_outlinks:
-                    set_of_outlinks.add(a)
+    # Begin crawl:
+    while crawled < target:
+        print('Wikipage retrieved: ' + wiki.title)
+        title_str = wiki.title.replace(',', ' ')
 
-            links_crawled[wiki.title] = new_page
+        # Check if page contains outlinks:
+        print('Crawled: ' + str(len(links_crawled)) + '\t\tOutlinks:', end=' ')
+        try:
+            print(str(len(wiki.links)))
+        except KeyError as e:
+            print('No outlinks available')
+            new_page = PageOutlinks([])
+            links_crawled[title_str] = new_page
             crawled += 1
 
-        if len(set_of_outlinks) == 0:
-            break
+        # Add to dictionary:
+        if wiki.title not in links_crawled:
+            new_page = PageOutlinks(wiki.links)
+            # 'links' method returns a list of internal links parsed from the page
+            # new_page constructs a WikiOutlinks object with a list argument
+            links_crawled[title_str] = new_page
+            # new_page is added to links_crawled dictionary
+            crawled += 1
 
-        # Select link to crawl
+        # Select a new link to crawl:
         search_term = random.choice(list(set_of_outlinks))
-        time.sleep(1.5)
+        print("Search: " + search_term)
+        new_term = False
+        while not new_term:
+            try:
+                wiki = wikipedia.page(search_term)
+                new_term = True
+            except wikipedia.exceptions.PageError as e:
+                li = wikipedia.search(search_term)
+                search_term = random.choice(li)
+            except wikipedia.exceptions.DisambiguationError as e:
+                li = wikipedia.search(search_term)
+                search_term = random.choice(li)
+            except wikipedia.exceptions.HTTPTimeoutError as e:
+                print("!!Timeout Error")
+                time.sleep(5)
+                # timeout error
+        time.sleep(.2)
 
-    #
-    # Sample output:
-    #
-    print("SAMPLE OUTPUT")
-    print(len(links_crawled))
-    for i in links_crawled:
-        print(i + ' - outlinks: ' + str(links_crawled[i].outlinks), end= ' ')
-        li = list(links_crawled[i].outlinks_dict)
-        print(li[:4], end=' ')
-        print('...etc')
-        print('\tINCLUDES KEY TERM? ', end=' ')
-        if key_term in li:
-            print('YES')
-        else:
-            print('NO')
-        print('\tINCLUDES DICTIONARY TERM? ', end=' ')
-        for a in li:
-            if a in links_crawled:
-                print('YES - ' + a, end=' ')
-                break
-        print()
-    #
-    #
+    # remove unneeded links from matrix
+    write_to_file(FILE_OUT, links_crawled)
+    links_reduced = strip_dict(links_crawled)
+    write_to_file(FILE_FILTER, links_reduced)
 
 
 def main():
